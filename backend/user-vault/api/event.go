36bcade24af121384df3d9bf97c1619908b9681c
yongj.zhuang <yongj.zhuang@outlook.com>
2024-10-07 20:44:51 +0800

    Disable LogPayload for notification pipelines

--- backend/user-vault/api/event.go
+++ backend/user-vault/api/event.go
@@ -4,12 +4,10 @@ import "github.com/curtisnewbie/miso/middleware/rabbit"
 
 var (
 	CreateNotifiPipeline = rabbit.NewEventPipeline[CreateNotifiEvent]("pieline.user-vault.create-notifi").
-				LogPayload().
 				MaxRetry(3).
 				Document("CreateNotifiPipeline", "Pipeline that creates notifications to the specified list of users", "user-vault")
 
 	CreateNotifiByAccessPipeline = rabbit.NewEventPipeline[CreateNotifiByAccessEvent]("pieline.user-vault.create-notifi.by-access").
-					LogPayload().
 					MaxRetry(3).
 					Document("CreateNotifiByAccessPipeline", "Pipeline that creates notifications to users who have access to the specified resource", "user-vault")
 )
