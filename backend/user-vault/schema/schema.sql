c33b4a1ba45ccdeed4c19f39e935999d00a361b7
yongj.zhuang <yongj.zhuang@outlook.com>
2025-09-10 23:34:35 +0800

    Polishing

--- backend/user-vault/schema/schema.sql
+++ backend/user-vault/schema/schema.sql
@@ -153,5 +153,21 @@ CREATE TABLE `site_password` (
   KEY `user_username_idx` (`user_no`,`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Personal passwords for different sites';
 
+CREATE TABLE note (
+    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+    `record_id` varchar(40) NOT NULL DEFAULT '' COMMENT 'record_id',
+    `title` text NULL DEFAULT NULL COMMENT 'title',
+    `content` mediumtext NULL DEFAULT NULL COMMENT 'content' ,
+    `user_no` varchar(32) NOT NULL DEFAULT '' COMMENT 'user no',
+    `created_at` datetime DEFAULT CURRENT_TIMESTAMP COMMENT 'created at',
+    `created_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'created by',
+    `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'updated at',
+    `updated_by` varchar(255) NOT NULL DEFAULT '' COMMENT 'updated by',
+    `deleted` tinyint(1) NOT NULL DEFAULT 0 COMMENT 'record deleted',
+    PRIMARY KEY (`id`),
+    KEY user_no_idx (user_no, deleted),
+    KEY record_id_idx (record_id, deleted)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='notes';
+
 -- default one for administrator, with this role, all paths can be accessed
 INSERT INTO user_vault.role(role_no, name) VALUES ('role_super_admin', 'Super Administrator');
