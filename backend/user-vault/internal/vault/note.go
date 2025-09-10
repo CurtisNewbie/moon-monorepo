d8f5a4a3a6d0e77d3565c9da09a4753a63480858
yongj.zhuang <yongj.zhuang@outlook.com>
2025-09-10 23:53:40 +0800

    More on note crud

--- backend/user-vault/internal/vault/note.go
+++ backend/user-vault/internal/vault/note.go
@@ -2,8 +2,10 @@ package vault
 
 import (
 	"github.com/curtisnewbie/miso/middleware/dbquery"
+	"github.com/curtisnewbie/miso/middleware/redis"
 	"github.com/curtisnewbie/miso/middleware/user-vault/common"
 	"github.com/curtisnewbie/miso/miso"
+	"github.com/curtisnewbie/miso/util/errs"
 	"github.com/curtisnewbie/miso/util/idutil"
 	"gorm.io/gorm"
 )
@@ -11,6 +13,14 @@ import (
 const (
 	TableNote = "note"
 )
+
+var (
+	ErrNoteNotFound = errs.NewErrfCode("NOTE_NOT_FOUND", "Note not found")
+)
+
+func NewNoteLock(rail miso.Rail, recordId string) *redis.RLock {
+	return redis.NewRLockf(rail, "user-vault:note:record-id:%v", recordId)
+}
 
 type SaveNoteReq struct {
 	Title   string
@@ -58,7 +68,7 @@ func DBUpdateNote(rail miso.Rail, db *gorm.DB, unr UpdateNoteReq, user common.Us
 		UpdateAny()
 }
 
-func DBUDeleteNote(rail miso.Rail, db *gorm.DB, recordId string, user common.User) error {
+func DBDeleteNote(rail miso.Rail, db *gorm.DB, recordId string, user common.User) error {
 	return dbquery.NewQuery(rail, db).
 		Table(TableNote).
 		SetCols(struct {
@@ -72,4 +82,76 @@ func DBUDeleteNote(rail miso.Rail, db *gorm.DB, recordId string, user common.Use
 		Eq("user_no", user.UserNo).
 		Eq("deleted", false).
 		UpdateAny()
+}
+
+type Note struct {
+	RecordId string
+	Title    string
+	Content  string
+	UserNo   string
+}
+
+func FindNote(rail miso.Rail, db *gorm.DB, recordId string, userNo string) (Note, error) {
+	var n Note
+	ok, err := dbquery.NewQuery(rail, db).
+		Table(TableNote).
+		SelectCols(n).
+		Eq("record_id", recordId).
+		Eq("user_no", userNo).
+		Eq("deleted", false).
+		ScanAny(&n)
+	if err != nil {
+		return n, err
+	}
+	if !ok {
+		return n, ErrNoteNotFound.New()
+	}
+	return n, nil
+}
+
+type ListNoteReq struct {
+	Keywords string
+	Paging   miso.Paging
+}
+
+func ListNotes(rail miso.Rail, db *gorm.DB, req ListNoteReq, user common.User) (miso.PageRes[Note], error) {
+	return dbquery.NewPagedQuery[Note](db).
+		WithBaseQuery(func(q *dbquery.Query) *dbquery.Query {
+			return q.Table(TableNote).
+				Eq("user_no", user.UserNo).
+				Eq("deleted", false).
+				WhereIf(req.Keywords != "", "title LIKE ? OR content LIKE ?", "%"+req.Keywords+"%") // TODO: replace like with match()
+		}).
+		WithSelectQuery(func(q *dbquery.Query) *dbquery.Query {
+			return q.SelectCols(Note{})
+		}).
+		Scan(rail, req.Paging)
+}
+
+func UpdateNote(rail miso.Rail, db *gorm.DB, req UpdateNoteReq, user common.User) error {
+	lock := NewNoteLock(rail, req.RecordId)
+	if err := lock.Lock(); err != nil {
+		return err
+	}
+	defer lock.Unlock()
+
+	_, err := FindNote(rail, db, req.RecordId, user.UserNo)
+	if err != nil {
+		return err
+	}
+	return DBUpdateNote(rail, db, req, user)
+}
+
+func DeleteNote(rail miso.Rail, db *gorm.DB, recordId string, user common.User) error {
+	lock := NewNoteLock(rail, recordId)
+	if err := lock.Lock(); err != nil {
+		return err
+	}
+	defer lock.Unlock()
+
+	_, err := FindNote(rail, db, recordId, user.UserNo)
+	if err != nil {
+		return err
+	}
+	return DBDeleteNote(rail, db, recordId, user)
 }
