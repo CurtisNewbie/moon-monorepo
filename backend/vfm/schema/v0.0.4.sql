93987183a462942b6ed0453594052322b90e7559
yongj.zhuang <yongj.zhuang@outlook.com>
2025-12-21 20:50:16 +0800

    Add idx

--- backend/vfm/schema/v0.0.4.sql
+++ backend/vfm/schema/v0.0.4.sql
@@ -1,1 +1,3 @@
 ALTER TABLE vfm.gallery_image ADD KEY gallery_name_idx (gallery_no, name), DROP KEY gallery_no_idx;
+
+ALTER TABLE vfm.file_info ADD KEY `parent_file_create_time_idx` (`parent_file`,`create_time`);
